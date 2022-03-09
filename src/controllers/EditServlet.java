0ea98b49066b153d906969a3e237c9ca4e73d70b
mio.nakagawa <mio_nkgw@yahoo.co.jp>
2022-03-09 18:33:07 +0900

    UpdateServlet.javaを作成

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -38,7 +38,9 @@ public class EditServlet extends HttpServlet {
         em.close();
 
         request.setAttribute("task", t);
-        request.setAttribute("_token", t.getId());
+        request.setAttribute("_token", request.getSession().getId());
+
+        request.getSession().setAttribute("task_id", t.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/tasks/edit.jsp");
         rd.forward(request, response);
