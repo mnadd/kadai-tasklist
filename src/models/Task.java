739fc0d2542a374c08eab483ea6aebd290b7b61c
mio.nakagawa <mio_nkgw@yahoo.co.jp>
2022-03-09 14:59:08 +0900

    Index/NewServletの修正

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,23 +7,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+    )
+})
 @Table(name = "tasks")
-public class tasks {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name = "update_at")
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    @Column(name = "content")
+    @Column(name = "content", length = 255, nullable = false)
     private String content;
 
     public Integer getId() {
@@ -57,5 +65,6 @@ public class tasks {
     public void setContent(String content) {
         this.content = content;
     }
+
 
 }
